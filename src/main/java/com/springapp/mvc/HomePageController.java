128d740a4a2de09127a5ba19b3680dde27405f98
Bagdi, Dalwinder (Associate Software Developer) <dalli_b@icloud.com>
2015-09-27 13:26:43 +0100

    Home Page Controller updated

--- src/main/java/com/springapp/mvc/HomePageController.java
+++ src/main/java/com/springapp/mvc/HomePageController.java
@@ -1,21 +1,20 @@
 package com.springapp.mvc;
 
-import com.sky.bean.ProductCatalogue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class HomePageController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView printWelcome() {
+	public String printWelcome() {
 		//model.addAttribute("message", "Hello world!");
-		ModelAndView model = new ModelAndView("index");
-		model.addObject("products", ProductCatalogue.getCatalogue().getProducts());
-		return model; //return a view
+//		ModelAndView model = new ModelAndView("index");
+//		model.addObject("products", ProductCatalogue.getCatalogue().getProducts());
+//		return model; //return a view
+		return "index";
 	}
 
 	// Add one attribute
